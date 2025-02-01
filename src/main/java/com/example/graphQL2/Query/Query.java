6239f429b6237f34c5ee969064a1c33dfe857827
pranay290491 <pranay.dmnsns@gmail.com>
2025-02-01 23:34:38 +0530

    Added Schema and few queries

---
+++ src/main/java/com/example/graphQL2/Query/Query.java
@@ -0,0 +1,22 @@
+package com.example.graphQL2.Query;
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+
+
+@Controller
+public class Query {
+
+    @QueryMapping
+    public String firstQuery(){
+        return " MY First Query";
+    }
+
+    @QueryMapping
+    public String secondQuery(){
+        return "My Second Query";
+    }
+}
